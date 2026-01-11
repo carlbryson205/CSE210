f1a90fe91a0097484d90399c30696231afb1e665
Bryson Carlton <carlbryson205@gmail.com>
2026-01-10 20:48:15 -0700

    finished prep1 and prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade? ");
+
+        string valueInText = Console.ReadLine();
+        int grade = int.Parse(valueInText);
+
+        string gradeLetter = "LOGIC FAIL";
+
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+            Console.WriteLine("You passed!");
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+            Console.WriteLine("You passed!");
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+            Console.WriteLine("You passed!");
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "D";
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            gradeLetter = "F";
+            Console.WriteLine("You failed!");
+        }
+
+        Console.WriteLine($"Your grade is: {gradeLetter}");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,50 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Console.WriteLine("What is your first name? ");
+        string fname = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+
+        Console.WriteLine("_______________________");
+
+
+        Console.WriteLine("What is your grade? ");
+
+        string valueInText = Console.ReadLine();
+        int grade = int.Parse(valueInText);
+
+        string gradeLetter = "LOGIC FAIL";
+
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+            Console.WriteLine("You passed!");
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+            Console.WriteLine("You passed!");
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+            Console.WriteLine("You passed!");
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "D";
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            gradeLetter = "F";
+            Console.WriteLine("You failed!");
+        }
+
+        Console.WriteLine($"Your grade is: {gradeLetter}");
     }
 }
